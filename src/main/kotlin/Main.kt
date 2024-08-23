354ee51e9d4df93a925a829610886fe2fe5c75fa
Vlad <vl.lad@MacBook-Pro-Vlad.local>
2024-08-23 13:33:28 +0300

    my_one_repository

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+
+   // вывод с переносом на новую строчку
+    println("Hello")
+    println("Kotlin")
+
+    // вывод в одну строку
+    print("Why?")
+    print("What?")
+}
